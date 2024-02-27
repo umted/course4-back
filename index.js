2de268618702b30effcd2af2745f10dafb4225f9
umted <98448169+umted@users.noreply.github.com>
2024-02-27 20:03:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors')
+
+const port = 3000;
+const app = express();
+const contacts = [{name: 'Telegram', link: 'https://t.me/umteddd'}, {name: 'VK', link: 'https://vk.com/umted'}]
+
+
+app.use(cors());
+
+
+
+app.get('/contacts', (req, res) => {
+    res.send(JSON.stringify(contacts));
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on http://localhost:${port}/`)
+})
